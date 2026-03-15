461453495268a1870ac996241b3737dc8644ee51
ashokonmi3 <ashokonmi3@gmail.com>
2026-03-15 13:37:23 +0530

    Add shell script to start Jenkins job

---
+++ shell_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting Jenkins Job"
+echo "--------------------"
+
+echo "Current user:"
+whoami
+
+echo "Current directory:"
+pwd
+
+echo "Files in workspace:"
+ls -l
+
+echo "Current date:"
+date
+
+echo "Job completed"
